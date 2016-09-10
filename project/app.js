8c8392c61905bb8f50ca269b0148e58a0305d8b7
Landon <w0304105@selu.edu>
2016-09-10 09:36:57 -0500

    Rebuilt some functions and objects to promote OOP and modular principles for future scaling. Reordered functions and labeled modules. No new gameplay functionality in this commit.

--- project/app.js
+++ project/app.js
@@ -15,58 +15,88 @@ var PLAYER_LIST = {};
 
 var WORLD_WIDTH = 500;
 var WINDOW_HEIGHT = 500;
-var MAP_SIZE = 5000;
+var MAP_SIZE = 1000;
 var PLAYER_SIZE = 50;
 
-var Player = function(identity) {
-	var self = {
-		x:Math.floor((MAP_SIZE - PLAYER_SIZE) * Math.random()),
-		y:Math.floor((MAP_SIZE - PLAYER_SIZE) * Math.random()),
-//		x:Math.floor(WINDOW_WIDTH * Math.random()),
-//		y:Math.floor(WINDOW_HEIGHT * Math.random()),
-		id:identity,
-		number:"" + Math.floor(10 * Math.random()),
-		pressLeft:false,
-		pressRight:false,
-		pressDown:false,
-		pressUp:false,
-		maxSpeed:5
-	}
-	self.updatePosition = function() {
-		if(self.pressLeft && self.x - self.maxSpeed > 0) {
-			self.x -= self.maxSpeed;
-		}
-		else if(self.pressRight && self.x + PLAYER_SIZE + self.maxSpeed < MAP_SIZE) {
-			self.x += self.maxSpeed;
-		}		
-		if(self.pressDown && self.y + PLAYER_SIZE + self.maxSpeed < MAP_SIZE) {
-			self.y += self.maxSpeed;
-		}
-		else if(self.pressUp && self.y - self.maxSpeed > 0) {
-			self.y -= self.maxSpeed;
-		}
-	}
+// ******** GAME LOOP **********************************
+setInterval(function() {
+	var playPackage = Player.update();
 	
-	return self;
-}
+	for(var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions', playPackage);
+	}
+}, 1000/25);
 
+// ******** SOCKET MODULE ******************************
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
 	socket.id = Math.random();
-	var sessionid = socket.id;
 	SOCKET_LIST[socket.id] = socket;
 	
-	socket.emit('init',{selfId:sessionid});
-	console.log("init " + sessionid);
-	
-	var player = Player(socket.id);
-	PLAYER_LIST[socket.id] = player;
-	
+	socket.emit('init',{selfId:socket.id});
+	console.log("init " + socket.id);
+		
+	Player.onConnect(socket);	
+		
 	socket.on('disconnect',function() {
 		delete SOCKET_LIST[socket.id];
-		delete PLAYER_LIST[socket.id];
+		Player.onDisconnect(socket);
 	});
+});
+
+// ******** PARENT OBJECT *******************************
+var Entity = function() {
+	var self = {
+		x:0,
+		y:0,
+		spdX:0,
+		spdY:0,
+		id:"",
+	}
+	self.update = function(){
+		self.updatePosition();
+	}
+	self.updatePosition = function(){
+		self.x += self.spdX;
+		self.y += self.spdY;
+	}
+	return self;
+}
+
+// ******** PLAYER MODULE ********************************
+var Player = function(id) {
+	var self = Entity();
+	self.id = id;
+	self.x = Math.floor((MAP_SIZE - PLAYER_SIZE) * Math.random());
+	self.y = Math.floor((MAP_SIZE - PLAYER_SIZE) * Math.random());
+	self.number = "" + Math.floor(10 * Math.random());
+	self.pressLeft = false;
+	self.pressRight = false;
+	self.pressDown = false;
+	self.pressUp = false;
+	self.playerSpeed = 5;
 	
+	self.updatePosition = function() {
+		if(self.pressLeft && self.x - self.playerSpeed > 0) {
+			self.x -= self.playerSpeed;
+		}
+		else if(self.pressRight && self.x + PLAYER_SIZE + self.playerSpeed < MAP_SIZE) {
+			self.x += self.playerSpeed;
+		}		
+		if(self.pressDown && self.y + PLAYER_SIZE + self.playerSpeed < MAP_SIZE) {
+			self.y += self.playerSpeed;
+		}
+		else if(self.pressUp && self.y - self.playerSpeed > 0) {
+			self.y -= self.playerSpeed;
+		}
+	}
+	Player.list[id] = self;
+	return self;
+}
+Player.list = {};
+Player.onConnect = function(socket){
+	var player = Player(socket.id);
 	socket.on('keyPress', function(data) {
 		if(data.inputId == 'left') {
 			player.pressLeft = data.state;
@@ -81,12 +111,14 @@ io.sockets.on('connection', function(socket) {
 			player.pressUp = data.state;
 		}
 	});
-});
-
-setInterval(function() {
+}
+Player.onDisconnect = function(socket){
+	delete Player.list[socket.id];
+}
+Player.update = function(){
 	var playPackage = [];
-	for(var i in PLAYER_LIST) {
-		var player = PLAYER_LIST[i];
+	for(var i in Player.list) {
+		var player = Player.list[i];
 		player.updatePosition();
 		playPackage.push({
 			x:player.x,
@@ -95,9 +127,8 @@ setInterval(function() {
 			speed:player.maxSpeed,
 		});
 	}
-	
-	for(var i in SOCKET_LIST) {
-		var socket = SOCKET_LIST[i];
-		socket.emit('newPositions', playPackage);
-	}
-}, 1000/25);
+	return playPackage;
+}
+
+// ******** BULLET MODULE ******************************
+
