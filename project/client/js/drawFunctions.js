1b7114548696ea47b9dddea8962f848d47083fcc
Christine Terranova <christine.cox@selu.edu>
2016-10-24 15:20:40 -0500

    Count players, friends and enemies on bigMap

--- project/client/js/drawFunctions.js
+++ project/client/js/drawFunctions.js
@@ -7,6 +7,37 @@
 		ctx.restore();	
 	}
 	
+	// Function for drawing players as circle for big map
+	function drawPlayers(playerX, playerY, pTeam) {
+		var xCoord = 0;
+		var yCoord = 0;
+		var hpColorN = "#990012";
+		var hpColorY = "#6cc417";
+		var color = "#FFFFFF";
+		
+		var scaleX = (clientWidth - 2 * borderSize)/(WORLD_WIDTH * MAP_WRAP_NUMBER);
+		var scaleY = (clientHeight - 2 * borderSize - divHeight)/(WORLD_HEIGHT * MAP_WRAP_NUMBER);
+		
+		xCoord = Math.floor(scaleX * (playerX + PLAYER_SIZE/2) + borderSize);
+		yCoord = Math.floor(scaleY * (playerY + PLAYER_SIZE/2) + borderSize);
+		
+		if(team === pTeam) {
+			color = hpColorY;
+			cntTeam0++;
+		}
+		else {
+			color = hpColorN;
+			cntTeam1++;
+		}
+		
+		ctx.beginPath();
+		ctx.arc(xCoord, yCoord, radius, 0, 2*Math.PI);
+		ctx.fillStyle = color;
+		ctx.fill();
+		ctx.strokeStyle = color;
+		ctx.stroke();
+	}	
+	
 	// Function for drawing the hp bar
 	function drawHP(botX, botY, team, p) {
 		var hpHeight = 7;
@@ -27,6 +58,7 @@
 		ctx.strokeRect(botX, botY, HP * hpWidth, hpHeight);
 	}
 	
+	// Resize window for game
 	function resizeFunction() {
 		clientWidth = window.innerWidth;
 		clientHeight = window.innerHeight;
@@ -38,4 +70,27 @@
 		var patrnBack = ctx.createPattern(Img.bgtile, "repeat");
 		ctx.fillStyle = patrnBack;
 		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	}
+	
+	// Resize window for big map
+	function resizeWindow() {
+		clientWidth = window.innerWidth - 2 * borderSize;
+		clientHeight = window.innerHeight - 2 * borderSize - divHeight;
+		ctx.canvas.width = clientWidth;
+		ctx.canvas.height = clientHeight;
+		playFit = Math.min(Math.floor(clientWidth/maxNumPlay), Math.floor(clientHeight/maxNumPlay));
+		radius = Math.max(fixRadius, playFit);	
+		ctx.font = '30px Arial';
+	}
+	
+	// Function to draw big map
+	function drawMap() {
+		ctx.clearRect(0, 0, clientWidth, clientHeight);	
+		ctx.fillStyle = "#E5E4E2";
+		ctx.fillRect(0, 0, clientWidth, clientHeight);
+		ctx.beginPath();
+		ctx.lineWidth = borderSize;
+		ctx.strokeStyle = "#000000";
+		ctx.rect(0, 0, clientWidth, clientHeight);
+		ctx.stroke();
 	}
