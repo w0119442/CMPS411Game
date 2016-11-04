ebc995c59e31dba7eee5e169e0d03204182cc6db
Christine Terranova <christine.cox@selu.edu>
2016-11-04 12:18:58 -0500

    Attempted to create a button pad for movement on mobile devices

--- project/client/js/mobile.js
+++ project/client/js/mobile.js
@@ -15,5 +15,61 @@ function detectmob() {
 }
 
 function mobileKeypad() {
+	mobileButtons.style.display = "block";
+	
+	mobileUpLeft.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:true});
+		socket.emit('keyPress',{inputId:'down', state:false});		
+		socket.emit('keyPress',{inputId:'left', state:true});
+		socket.emit('keyPress',{inputId:'right', state:false});		
+	}
+	mobileUp.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:true});
+		socket.emit('keyPress',{inputId:'down', state:false});		
+		socket.emit('keyPress',{inputId:'left', state:false});
+		socket.emit('keyPress',{inputId:'right', state:false});		
+	}
+	mobileUpRight.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:true});
+		socket.emit('keyPress',{inputId:'down', state:false});		
+		socket.emit('keyPress',{inputId:'left', state:false});
+		socket.emit('keyPress',{inputId:'right', state:true});		
+	}
+	mobileLeft.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:false});
+		socket.emit('keyPress',{inputId:'down', state:false});		
+		socket.emit('keyPress',{inputId:'left', state:true});
+		socket.emit('keyPress',{inputId:'right', state:false});		
+	}
+	mobileRight.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:false});
+		socket.emit('keyPress',{inputId:'down', state:false});		
+		socket.emit('keyPress',{inputId:'left', state:false});
+		socket.emit('keyPress',{inputId:'right', state:true});		
+	}
+	mobileDownLeft.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:false});
+		socket.emit('keyPress',{inputId:'down', state:true});		
+		socket.emit('keyPress',{inputId:'left', state:true});
+		socket.emit('keyPress',{inputId:'right', state:false});		
+	}
+	mobileDown.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:false});
+		socket.emit('keyPress',{inputId:'down', state:true});		
+		socket.emit('keyPress',{inputId:'left', state:false});
+		socket.emit('keyPress',{inputId:'right', state:false});		
+	}
+	mobileDownRight.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:false});
+		socket.emit('keyPress',{inputId:'down', state:true});		
+		socket.emit('keyPress',{inputId:'left', state:false});
+		socket.emit('keyPress',{inputId:'right', state:true});		
+	}	
+	mobileNull.onclick = function() {
+		socket.emit('keyPress',{inputId:'up', state:false});
+		socket.emit('keyPress',{inputId:'down', state:false});		
+		socket.emit('keyPress',{inputId:'left', state:false});
+		socket.emit('keyPress',{inputId:'right', state:false});		
+	}
 	
 }
