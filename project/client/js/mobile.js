dcdc92dbfea2c4aa73f812738decce2c8547b458
Christine Terranova <christine.cox@selu.edu>
2016-11-11 14:24:09 -0600

    Detects mobile devices now. Still need to fix height and width of images and buttons

--- project/client/js/mobile.js
+++ project/client/js/mobile.js
@@ -1,17 +1,10 @@
-function detectMob() { 
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
-    return true;
-  }
- else {
-    return false;
-  }
+function detectMob() {
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 function mobileKeypad() {
